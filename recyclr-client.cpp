25271d2de5bd6381afe8a7e8dd0550bb4ac61189
Patrick Caruso <patrickec246@gmail.com>
2020-01-20 23:15:20 +0000

    Get that name outta here

--- recyclr-client.cpp
+++ recyclr-client.cpp
@@ -24,14 +24,14 @@ RecyclrClient::RecyclrClient() :
 RecyclrClient::~RecyclrClient()
 {
     if (horizontal_fd != -1) {
-        if (close(horizontal_fd)) {
+        if (::close(horizontal_fd)) {
             cout << "ERROR: Unable to close horizontal file descriptor: " << horizontal_fd << "\n";
             // TODO: Throw once we build out exceptions
         }
     }
 
     if (vertical_fd != -1) {
-        if (close(vertical_fd)) {
+        if (::close(vertical_fd)) {
             cout << "ERROR: Unable to close vertical file descriptor: " << vertical_fd << "\n";
             // TODO: Throw once we build out exceptions
         }
