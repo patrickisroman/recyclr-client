6efa0fdc6c8a64701154833c91af4c52b09ac1a7
Patrick Caruso <patrickec246@gmail.com>
2020-01-22 11:33:23 -0800

    Throw warning for MacOSx affinity methods

--- recyclr-utils.cpp
+++ recyclr-utils.cpp
@@ -16,16 +16,26 @@ u64 millis()
 
 u32 set_thread_affinity(std::thread* thr, thread_mask affinity)
 {
-    cpu_set_t cpu;
-    CPU_ZERO(&cpu);
-    CPU_SET(affinity, &cpu);
-    return pthread_setaffinity_np(thr->native_handle(), sizeof(cpu), &cpu);
+    #ifdef __APPLE__
+        #warning CPU Affinity not supported for MacOSx. Build will continue...
+        return 0;
+    #else
+        cpu_set_t cpu;
+        CPU_ZERO(&cpu);
+        CPU_SET(affinity, &cpu);
+        return pthread_setaffinity_np(thr->native_handle(), sizeof(cpu), &cpu);
+    #endif
 }
 
 u32 set_process_affinity(thread_mask affinity)
 {
-    cpu_set_t cpu;
-    CPU_ZERO(&cpu);
-    CPU_SET(affinity, &cpu);
-    return sched_setaffinity(0, sizeof(cpu), &cpu);
+    #ifdef __APPLE__
+        #warning CPU Affinity not supported for MacOSx. Build will continue...
+        return 0;
+    #else
+        cpu_set_t cpu;
+        CPU_ZERO(&cpu);
+        CPU_SET(affinity, &cpu);
+        return sched_setaffinity(0, sizeof(cpu), &cpu);
+    #endif
 }
